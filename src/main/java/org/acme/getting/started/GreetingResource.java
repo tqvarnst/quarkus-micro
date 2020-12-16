85c838cfa6cf045e76d8ea06c43f71128c74029b
Thomas Qvarnström <tqvarnst@redhat.com>
2020-12-16 16:58:44 +0100

    v1.0

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -3,6 +3,7 @@ package org.acme.getting.started;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -15,7 +16,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
-    public String greeting(String name) {
+    public String greeting(@PathParam("name") String name) {
         return service.greeting(name);
     }
 
